9442c7fe02f9dbc27b731af92c6b411f5a0e8197
tasajos <tasajos@gmail.com>
2023-04-02 08:51:38 -0400

    instalar firebase

---
+++ Cfirebase/readmetxt.txt
@@ -0,0 +1,6 @@
+install npm install
+firebaselogin
+firebase projects:lists
+firebase init
+ng add @angular/fire
+
---
+++ Cfirebase/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'crudfb-4ffbe',
+    appId: '1:928475873492:web:f7e42228ef9a08bcae3b41',
+    storageBucket: 'crudfb-4ffbe.appspot.com',
+    apiKey: 'AIzaSyAhc3uIsiOXHG4aHXYK5eR21gP-acDW-_8',
+    authDomain: 'crudfb-4ffbe.firebaseapp.com',
+    messagingSenderId: '928475873492',
+  },
+    production: false,
+    endpoint: 'https://localhost:7054/'
+    
+};
